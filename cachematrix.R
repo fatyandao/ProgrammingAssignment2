958580462845f67c4373c692a464a5960bbfed15
Clarence Zeng <fatyandao@gmail.com>
2014-09-17 13:37:09 +0800

    Ok, so basically I just edited the code that caches the mean in the assignment, and change it to calculate the inverse.
    Nearly got the whole thing wrong.
    
    Well, here it is.

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function just asssigns x to a cachable variable and returns this variable
+## This function create a special variable that can store 2 sub-variables
+## the original matrix, and the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  hoseyboh <<- x
-  return(hoseyboh)
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse){ s <<- inverse}
+        getinverse <- function() s
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## This function uses the solve() function to get the inverse of x. 
-## If the inverse is already calculated, do not calculate again. 
+## This function uses the solve() function to get the inverse of the matrix inside x, 
+## And then stores it in the inverse variable of x.
+## If the inverse is already cached in x, do not calculate and just return the inverse of x
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  if (identical(origin_x,x)
-      return (solved)
-      else
-        origin_x <<- x
-      solved <<- solve(x)
-      return (solved)  
+        s <- x$getinverse()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
 }
+
