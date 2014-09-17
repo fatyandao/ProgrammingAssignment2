adfc291b26125d5a7b8113fcb492d71e9fa83c84
Clarence Zeng <fatyandao@gmail.com>
2014-09-17 13:02:07 +0800

    Added in the functions, not sure if it'll work though.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function just asssigns x to a cachable variable and returns this variable
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  hoseyboh <<- x
+  return(hoseyboh)
 }
 
 
-## Write a short comment describing this function
+## This function uses the solve() function to get the inverse of x. 
+## If the inverse is already calculated, do not calculate again. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if (identical(origin_x,x)
+      return (solved)
+      else
+        origin_x <<- x
+      solved <<- solve(x)
+      return (solved)  
 }
